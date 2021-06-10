61555416e76fe8a456676f058d3c4b035ff5c78b
Renato Zimmermann <renatomatz@gmail.com>
2021-06-10 17:12:40 -0400

    added 1D versions

--- src/otf.f90
+++ src/otf.f90
@@ -8,7 +8,7 @@ module otf
 
 contains
 
-    pure function auckley(x, a, b, c) result(fx)
+    pure function auckley_2d(x, a, b, c) result(fx)
         real(kind=wp), dimension(:,:), intent(in) :: x
         real(kind=wp), intent(in) :: a, b, c
         real(kind=wp), dimension(size(x, 1)) :: fx
@@ -19,9 +19,9 @@ contains
         fx = -a*exp(-b*sqrt((1.d0/d)*sum(x**2, 2)))&
              - exp((1.d0/d)*sum(cos(c*x), 2))&
              + a + exp(1.d0)
-    end function auckley
+    end function auckley_2d
 
-    pure function rosenbrock(x) result(fx)
+    pure function rosenbrock_2d(x) result(fx)
         real(kind=wp), dimension(:,:), intent(in) :: x
         real(kind=wp), dimension(size(x, 1)) :: fx
 
@@ -37,9 +37,9 @@ contains
         x2 = x(:,2:)
 
         fx = sum(100*(x2-x1**2)**2+(x1-1)**2, 2)
-    end function rosenbrock
+    end function rosenbrock_2d
 
-    pure function eggholder(x) result(fx)
+    pure function eggholder_2d(x) result(fx)
         real(kind=wp), dimension(:,:), intent(in) :: x
         real(kind=wp), dimension(size(x, 1)) :: fx
 
@@ -56,9 +56,9 @@ contains
 
         fx = -(x2+47)*sin(sqrt(abs(x2+(x1/2)+47))) &
                   -x1*sin(sqrt(abs(x1-(x2+47))))
-    end function eggholder
+    end function eggholder_2d
 
-    pure function cross_in_tray(x) result(fx)
+    pure function cross_in_tray_2d(x) result(fx)
         real(kind=wp), dimension(:,:), intent(in) :: x
         real(kind=wp), dimension(size(x, 1)) :: fx
 
@@ -78,9 +78,9 @@ contains
                 100 - (sqrt(x1**2 + x2**2)/pi) &
             )) &
         )+1)**0.1
-    end function cross_in_tray
+    end function cross_in_tray_2d
 
-    pure function griewank(x) result(fx)
+    pure function griewank_2d(x) result(fx)
         real(kind=wp), dimension(:,:), intent(in) :: x
         real(kind=wp), dimension(size(x, 1)) :: fx
 
@@ -99,6 +99,57 @@ contains
              - product(cos( &
                 x/sqrt(seq) &
              ), 2) + 1
-    end function griewank
+    end function griewank_2d
+
+    pure function auckley_1d(x, a, b, c) result(fx)
+        real(kind=wp), dimension(:), intent(in) :: x
+        real(kind=wp), intent(in) :: a, b, c
+        real(kind=wp) :: fx
+
+        real(kind=wp), dimension(size(x, 1)) :: fx_res
+
+        fx_res = auckley_2d(reshape(x, [1, size(x)]), a, b, c)
+        fx = fx_res(1)
+    end function auckley_1d
+
+    pure function rosenbrock_1d(x) result(fx)
+        real(kind=wp), dimension(:,:), intent(in) :: x
+        real(kind=wp) :: fx
+
+        real(kind=wp), dimension(size(x, 1)) :: fx_res
+
+        fx_res = rosenbrock_2d(reshape(x, [1, size(x)]))
+        fx = fx_res(1)
+    end function rosenbrock_1d
+
+    pure function eggholder_1d(x) result(fx)
+        real(kind=wp), dimension(:,:), intent(in) :: x
+        real(kind=wp) :: fx
+
+        real(kind=wp), dimension(size(x, 1)) :: fx_res
+
+        fx_res = eggholder_2d(reshape(x, [1, size(x)]))
+        fx = fx_res(1)
+    end function eggholder_1d
+
+    pure function cross_in_tray_1d(x) result(fx)
+        real(kind=wp), dimension(:,:), intent(in) :: x
+        real(kind=wp) :: fx
+
+        real(kind=wp), dimension(size(x, 1)) :: fx_res
+
+        fx_res = cross_in_tray_2d(reshape(x, [1, size(x)]))
+        fx = fx_res(1)
+    end function cross_in_tray_1d
+
+    pure function griewank_1d(x) result(fx)
+        real(kind=wp), dimension(:,:), intent(in) :: x
+        real(kind=wp) :: fx
+
+        real(kind=wp), dimension(size(x, 1)) :: fx_res
+
+        fx_res = griewank_2d(reshape(x, [1, size(x)]))
+        fx = fx_res(1)
+    end function griewank_1d
 
 end module otf
